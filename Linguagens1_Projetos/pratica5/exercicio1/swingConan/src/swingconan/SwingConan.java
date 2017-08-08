f3376386a2901f9b9dd7f23d817712e7f4b595f9
fe <fetpiva@gmail.com>
2017-08-08 11:12:33 -0300

    \sdghfuosd

---
+++ Linguagens1_Projetos/pratica5/exercicio1/swingConan/src/swingconan/SwingConan.java
@@ -0,0 +1,37 @@
+
+package swingconan;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class SwingConan {
+
+    
+    public static void main(String[] args) {
+        
+      /* JFrame telaPrincipal = new JFrame();
+       telaPrincipal.setVisible(true);
+       telaPrincipal.setSize(300, 500);
+       JLabel bla= new JLabel("asudgisdf");
+       bla.setVisible(true);
+       telaPrincipal.add(bla);
+       
+       */
+      
+      TelaBonita tela = new TelaBonita();
+      tela.setVisible(true);
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+               
+    }
+    
+}
