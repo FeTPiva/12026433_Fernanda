8db7303f3548b89756583395f3f43056f9e966c9
Aluno <Aluno>
2017-08-16 11:23:35 -0300

    ahsdfjisdhf

--- Linguagens1_Projetos/pratica6/exercicio1/Tela/src/tela/FXMLDocumentController.java
+++ Linguagens1_Projetos/pratica6/exercicio1/Tela/src/tela/FXMLDocumentController.java
@@ -11,25 +11,57 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import java.io.File;
 
-/**
- *
- * @author Aluno 07
- */
 public class FXMLDocumentController implements Initializable {
-    
+
+    private static final String FILENAME = "T:\\linguagens\\12026433\\12026433_Fernanda\\"
+            + "Linguagens1_Projetos\\pratica6\\exercicio1\\bancodds.txt";
+
     @FXML
-    private Label label;
-    
+    private Button btnSalvar;
+    @FXML
+    private TextField nField;
+    @FXML
+    private TextField eField, iField, rgField, cpfField;
+    @FXML
+    private ImageView img;
+    @FXML
+    private Label test;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+
+        // 
+        try {
+            File file = new File("T:\\linguagens\\12026433\\12026433_Fernanda\\Linguagens1_Projetos\\pratica6\\exercicio1\\bancodds.txt");
+
+           file.createNewFile();
+           
+          String content = nField.getText() + " " + iField.getText() + " " + eField.getText()
+            + " " + cpfField.getText() + " " + rgField.getText();; 
+           
+           
+           
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
+        
+        test.setText("e ai mew");
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
