765d22ca8c4629e9103ae5f1dc7f992daf7cafc0
Aluno <Aluno>
2017-09-05 11:15:21 -0300

    ihasdfohsdf

---
+++ Linguagens1_Projetos/pratica9/ex1/TesteErro/src/testeerro/TesteErro.java
@@ -0,0 +1,38 @@
+package testeerro;
+
+public class TesteErro {
+
+    public static void main(String[] args) {
+
+        System.out.println("inicio do main");
+        metodo1();
+        System.out.println("fim do main");
+    }
+
+    static void metodo1() {
+        System.out.println("inicio do metodo1");
+        try {
+            metodo2();
+        } catch (Exception e) {
+            System.out.println("meu erro 2: " + e);
+
+        }
+        System.out.println("fim do metodo1");
+    }
+
+    static void metodo2() throws ArrayIndexOutOfBoundsException{
+        System.out.println("inicio do metodo2");
+        int[] array = new int[10];
+ try {
+        for (int i = 0; i <= 15; i++) {
+           
+                array[i] = i;
+                System.out.println(i);
+           
+        }
+ } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("meu erro: " + e);
+            }
+        System.out.println("fim do metodo2");
+    }
+}
